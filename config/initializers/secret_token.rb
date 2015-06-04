8dc14078742a5a07d40249e5ce7fdd5a9078de9f
joey gibson <joe.vgibson@gmail.com>
2015-06-03 19:54:57 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unchained::Application.config.secret_token = '716810cfdaa15050cdb836c5994642ce3e5ef1e5d09c73a43de1b897954748554e75a6e2d6112eaa485ee8775cb1e7276022000326e94540787b0ead5311d6c5'
